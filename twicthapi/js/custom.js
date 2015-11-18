4baff276b7c2b51c301be56dbc7fb757848e1503
kairath <thekairath@hotmail.com>
2015-11-18 17:47:43 +0000

    weather app finished

--- twicthapi/js/custom.js
+++ twicthapi/js/custom.js
@@ -1,31 +1,26 @@
 $(function(){
-  var apikey = "24e75b084aa0de4b90f0cffc6fe57bcd", lat, lon;
   var streamers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb",
                     "thomasballinger","noobs2ninjas","beohoff", "medrybw"];
 
   var x;
   for(var i = 0; i < streamers.length; i++){
-    var status, streamer = streamers[i], uhtml, tburl;
+    var status, streamer = streamers[i], tburl;
     tburl = "https://api.twitch.tv/kraken/streams/" + streamer;
       $.ajax({
         dataType: "json",
         url: tburl,
+        method: "GET",
         success: function(data){
+          var status;
+          var uhtml;
           if( data.stream ){
             status = "online";
           } else { status = "offline"; }
-          uhtml = "<div class='user'" + i +"><a href='http://www.twitch.tv/'></a>";
-          uhtml += "<p class='status'></p></div>";
-          
-        },
-        method: "GET"
+          uhtml = "<div class='user" + i +"'><a href='http://www.twitch.tv/" + streamers[i] +"'>" + streamers[i] + "</a>";
+          uhtml += "<p class='status'>" + status + "</p></div>";
+          $(".users").append(uhtml);
+        }
       });
-    
-    }
-    for(var j = 0; j < streamers.length; i++){
-
-      $(".users a").attr("href",streamers[j]);
-      $(".users a").text("href",streamers[j]);
     
     }
 });
--- weatherapp/js/custom.js
+++ weatherapp/js/custom.js
@@ -1,24 +1,29 @@
 $(function(){
-  var apikey, lat, lon;
+  var apikey = "24e75b084aa0de4b90f0cffc6fe57bcd", lat, lon, keeperofdata;
   var streamers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
     $("#test").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
+    lat = position.coords.latitude.toFixed(2);
+    lon = position.coords.longitude.toFixed(2);
     });
   }
   
-  var wurl = "http://api.openweathermap.org/data/2.5/weather?lat=" + 39 + "&lon=" + 32 + "&appid=" + apikey;
+  var wurl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apikey;
   $.ajax({
     method: "GET",
     dataType: "json",
     url: wurl,
     success: function(data){
-      $("#test").text(data)
+      keeperofdata = data;
+      $(".weath p").text(data.weather[0].description);
+      $(".temp p").text(data.main.temp);
+      $(".city p").text(data.sys["country"]);
     }
   });
+
+  $("#test").text(lon);
   
 
 });
