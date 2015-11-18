67b628bf30ae2364b7ad5dd8172342a09777dad9
kairath <thekairath@hotmail.com>
2015-11-18 22:05:08 +0000

    twitch api skeleton finished.need some layout design

--- twicthapi/js/custom.js
+++ twicthapi/js/custom.js
@@ -1,27 +1,50 @@
 $(function(){
   var streamers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb",
-                    "thomasballinger","noobs2ninjas","beohoff", "medrybw"];
+                    "thomasballinger","noobs2ninjas","beohoff", "medrybw", "comster404"];
 
   var x;
-  for(var i = 0; i < streamers.length; i++){
-    var status, streamer = streamers[i], tburl;
-    tburl = "https://api.twitch.tv/kraken/streams/" + streamer;
-      $.ajax({
-        dataType: "json",
-        url: tburl,
-        method: "GET",
-        success: function(data){
-          var status;
-          var uhtml;
-          if( data.stream ){
-            status = "online";
-          } else { status = "offline"; }
-          uhtml = "<div class='user" + i +"'><a href='http://www.twitch.tv/" + streamers[i] +"'>" + streamers[i] + "</a>";
-          uhtml += "<p class='status'>" + status + "</p></div>";
-          $(".users").append(uhtml);
-        }
-      });
-    
+  $.each(streamers, function (i, item) {
+    var uhtml;
+    var status;
+    $.ajax({
+      dataType: "json",
+      url: "https://api.twitch.tv/kraken/streams/" + item,
+      method: "GET",
+      success: function(data){
+        if( !!data.stream == false) { status = "offline"; }
+        
+        uhtml = "<div class='user" + i +"'><a href='http://www.twitch.tv/" + item +"' target='_blank'>" + item + "</a>";
+        if( data.stream ){ status = "online"; uhtml += "<span class='streaminfo'>" + data.stream.game + "</span>"} 
+        uhtml += "<p class='" +status +"'>" + status + "</p></div>";
+        $(".users").append(uhtml);
+      }
+    });
+  });
+
+  $(".buttondiva").on("click", function(){
+    $(".users > div").removeClass("hideit");
+  });
+
+  $(".buttondivon").on("click", function(){
+    $(".users > div").addClass("hideit")
+    $(".online").parent("div").removeClass("hideit")
+  });
+
+  $(".buttondivof").on("click", function(){
+    $(".users > div").addClass("hideit")
+     $(".offline").parent("div").removeClass("hideit")
+  });
+
+  $(".searchi").on("keydown",function(){
+    $tval = $(this).val();
+    for(var i=0; i < streamers.length; i++){
+      if( $tval.length == 1){ $(".users > div").removeClass("hideit") }
+      if ( streamers[i].indexOf($tval) == -1 ){
+        $(".user" + i).addClass("hideit");
+      }
     }
+  })
+
+
 });
 
