09ff989ae663cc55a74b69e54d9a02780363e294
kairath <thekairath@hotmail.com>
2015-11-22 10:07:22 +0000

    promodoro fully functional-animation might be added in future

--- champernews/js/custom.js
+++ champernews/js/custom.js
@@ -1,27 +1,30 @@
 $(function(){
 
     var cnurl = "http://www.freecodecamp.com/news/hot/";
-    var datar;
+    var chtml = "";
+    var timeconverter = function(n){
+      var t = new Date(n) + "";
+      return t.slice(4, 15);
+    }
     $.ajax({
       dataType: "json",
       url: cnurl,
       success: function(data){
-        datar = data;
+        data = data.filter(function(item, pos) {
+          return data.indexOf(item) == pos;
+        })
+        for(var i = 0; i < data.length; i++){
+
+         chtml += "<div class='user'><a href='http://www.freecodecamp.com/" + data[i].author.username + "' target='_blank'><img src='" + data[i].author.picture + "'/></a>"
+         chtml += "<a href='" + data[i].link + "' target='_blank'><p>" + data[i].metaDescription.substr(0,24) + "...</p></a>"
+         chtml += "<span>By -" + data[i].author.username + "<span>  +" + data[i].upVotes.length + "</span>"
+         chtml += "<p>Posted on: " +  timeconverter(data[i].timePosted) + "</p></div>" 
+
+         $(".users").append(chtml);
+       }
       },
       method: "GET"
     });
-    
-    for(var i = 0; i< datar.length; i++){
-      var html;
-      html = "<div class='user'>< href='" + datar[i].link + "'<img src='" +
-      <div class="user">
-          <img src="https://avatars.githubusercontent.com/u/15031730">
-          <a href="#">asdasdasd asdasd</a>
-          <span class="username">asdfds</span><span class=likes>  +3</span>
-          <p>Posted on: asdas dsad ds</p>
-          </div>
-    }
-    
     
     
 });
