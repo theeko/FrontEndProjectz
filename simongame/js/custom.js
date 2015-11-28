0bcd156fcf8eeff2445b5b6111a895e0f63d20d7
kairath <thekairath@hotmail.com>
2015-11-28 14:18:00 +0000

    added some more css and some js to simon game

--- simongame/js/custom.js
+++ simongame/js/custom.js
@@ -1,3 +1,43 @@
 $(function(){
+  
+  var game = false;
+  
+  $(".onoff").on("click", function(){
+    $(".on").toggleClass("active");
+    $(".off").toggleClass("active");
+    $(".on").toggleClass("passive");
+    $(".off").toggleClass("passive");
+    if(game == true){ game = false;} 
+    if(game == false){ game = true; } //game always true
+  });
 
+  $(".strict").on("click", function(){
+    $(".strictlamb").toggleClass("slamb"); //class not added but click works
+  });
+  $(".uleft").on("click", function(){
+     $(".uleft").addClass("ulbright"); 
+     setTimeout(function(){
+       $(".uleft").removeClass("ulbright");
+     },300);
+  });
+  $(".uright").on("click", function(){
+     $(".uright").addClass("urbright"); 
+     setTimeout(function(){
+       $(".uright").removeClass("urbright");
+     },300);
+  });
+  $(".bleft").on("click", function(){
+     $(".bleft").addClass("blbright"); 
+     setTimeout(function(){
+       $(".bleft").removeClass("blbright");
+     },300);
+  });
+  $(".bright").on("click", function(){
+     $(".bright").addClass("brbright"); 
+     setTimeout(function(){
+       $(".bright").removeClass("brbright");
+     },300);
+  });
+
+  
 });
